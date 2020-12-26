9789dd38149c1625ba2cd9ce07d17b07945846bc
Aditya Rajput <rajaditya3998@gmail.com>
2020-12-26 18:44:42 +0530

    Final Commit

---
+++ blog/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from taggit.models import Tag
+
+
+# Create your views here.
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug = tag_slug)
+        object_list = object_list.filter(tag__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                'blog/post/list.html',
+                {'page' : page,
+                'posts' : posts,
+                'tag' : tag})
+                
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+
+
+    #List of active Comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data = request.POST)
+        if comment_form.is_valid():
+            #Create Comment Object but don't save to database yet
+            new_comment = comment_form.save(commit = False)
+
+            #Assign the current post to the comment
+            new_comment.post = post
+
+            # Save the comment to the Database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,
+                'blog/post/detail.html',
+                {'post': post,
+                'comments' : comments,
+                'new_comment' : new_comment,
+                'comment_form' : comment_form})
+
+def post_share(request, post_id):
+    # Retrieves posts by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['Comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            send = True
+
+
+            # Send Mail
+    else:
+            form = EmailPostForm()
+    return render(request, 'blog/post/share.html',{'post' : post,
+                                                'form': form,
+                                                'sent': sent})
